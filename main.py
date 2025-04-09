8a2b6a7f343e890855cce76022764cbde3730761
pndaRN <markbwilliamsjr@gmail.com>
2025-04-09 06:39:30 -0500

    drawing player and player movement

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         pygame.Surface.fill(screen,(0,0,0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        #refreshes screen
         pygame.display.flip()
+
+        #limit framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
+        
+
+        
+       
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
